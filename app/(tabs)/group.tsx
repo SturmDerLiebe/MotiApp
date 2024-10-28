052a48381a6ae45349e9e15556614402ef50f58c
Christian Sturm <christian.sturm@code.berlin>
2024-10-28 19:00:42 +0100

    feat: implement new components

--- app/(tabs)/group.tsx
+++ app/(tabs)/group.tsx
@@ -1,34 +1,39 @@
-import { FlashList } from "@shopify/flash-list";
-import { View, Text } from "react-native";
+import { Colors } from "@/constants/Colors";
+import { Fonts } from "@/constants/Fonts";
 import { ChatMessage, ImageMessage, TextMessage } from "@/data/ImageMessage";
+import { FlashList } from "@shopify/flash-list";
+import { Image } from "expo-image";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function IndexScreen() {
+const CHAT_STYLES = StyleSheet.create({
+  chatContainer: { alignItems: "stretch" },
+  messageContainer: { backgroundColor: Colors.grey.light2 },
+  image: { borderRadius: 50, height: "25%" },
+});
+
+export default function GroupScreen() {
   return (
-    <View
-      style={[
-        {
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "flex-start",
-          paddingStart: 12,
-        },
-        TestStyles,
-      ]}
-    >
+    <View>
       <FlashList
-        data={DATED_SAMPLE_DATA}
+        style={CHAT_STYLES.chatContainer}
+        data={SAMPLE_DATA_WITH_DATE}
         renderItem={function ({ item, index }) {
-          if (typeof item == "string") {
-            // TODO: Treat first item always being a date
+          function itemIsDateString(item: ChatDataItem): item is string {
+            return typeof item === "string";
+          }
 
+          function determinePreviousAuthor() {
+            let previousItem = SAMPLE_DATA_WITH_DATE[index - 1];
+            return typeof previousItem == "string" ? null : previousItem.author;
+          }
+
+          if (itemIsDateString(item)) {
             return <Text>{item}</Text>;
           } else {
-            let previousItem = DATED_SAMPLE_DATA[index - 1];
-            let previousAuthor =
-              typeof previousItem == "string" ? null : previousItem.author;
-            let currentAuthor = item.author;
-
-            return item.renderMessage(currentAuthor !== previousAuthor);
+            return determineComponentByMessageType(
+              item,
+              item.author !== determinePreviousAuthor(),
+            );
           }
           //TODO: Check if last two entries should display "Today" or "Yesterday""
         }}
@@ -39,11 +44,60 @@ export default function IndexScreen() {
   );
 }
 
+export function determineComponentByMessageType(
+  item: ChatMessage,
+  shouldShowAuthor: boolean,
+) {
+  if (item instanceof TextMessage) {
+    return (
+      <TextMessageComponent shouldRenderAuthor={shouldShowAuthor} {...item} />
+    );
+  } else if (item instanceof ImageMessage) {
+    return (
+      <Image
+        source={item.imageURL}
+        contentFit="scale-down"
+        style={CHAT_STYLES.image}
+      />
+    );
+  } else {
+    return null;
+  }
+}
+
+export function TextMessageComponent(props: {
+  text: string;
+  timeString: string;
+  author: string;
+  shouldRenderAuthor?: boolean;
+}) {
+  return (
+    <View style={CHAT_STYLES.messageContainer}>
+      {props.shouldRenderAuthor ? (
+        <Text style={Fonts.paragraph.p9}>{props.author}</Text>
+      ) : null}
+
+      <Text style={Fonts.paragraph.p4}>{props.text}</Text>
+
+      <Text style={Fonts.date.small}>{props.timeString}</Text>
+    </View>
+  );
+}
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
+// --------------------
 const SAMPLE_DATA = [
   new ImageMessage(
     "Jung",
     "2024-08-11T17:03:06Z",
-    "../../assets/images/SampleImages/SampleImageMessage.png",
+    "https://placehold.co/140x184",
   ),
   new TextMessage(
     "Jung",
@@ -77,35 +131,36 @@ const SAMPLE_DATA = [
   ),
 ];
 
-function groupByPolyFill<T>(
-  arr: T[],
-  callback: (currentElement: T, index: number) => string | symbol,
-) {
+function groupByPolyFill<ItemType, Key extends string | symbol>(
+  arr: ItemType[],
+  callback: (currentElement: ItemType, index: number) => Key,
+): Record<Key, ItemType[]> {
   return arr.reduce(
-    (
-      acc: Record<string | symbol, T[]> = {},
-      currentElement: T,
-      index: number,
-    ) => {
+    (acc: Record<Key, ItemType[]>, currentElement: ItemType, index: number) => {
       const key = callback(currentElement, index);
       acc[key] ??= [];
       acc[key].push(currentElement);
       return acc;
     },
-    {},
+    {} as Record<Key, ItemType[]>,
   );
 }
 
 // TODO: Concat Groups and splice with date Segment
 const GROUPED_SAMPLE_DATA = groupByPolyFill(SAMPLE_DATA, function (item) {
   return item.dateString;
-}) as Record<string, (ImageMessage | TextMessage)[]>; // NOTE: groupBy returns Partial even though all groups are required in this case
+});
 
-const DATED_SAMPLE_DATA = Object.entries(GROUPED_SAMPLE_DATA).reduce(function (
-  accumulatingArray: (ImageMessage | TextMessage | string)[],
+type ChatDataItem = ChatMessage | string;
+
+const SAMPLE_DATA_WITH_DATE: ChatDataItem[] = Object.entries(
+  GROUPED_SAMPLE_DATA,
+).reduce(function (
+  accumulatingArray: ChatDataItem[],
   [currentDateString, currentMessages],
 ) {
   accumulatingArray.push(currentDateString, ...currentMessages);
   return accumulatingArray;
 }, []);
+
 export const TestStyles = { borderColor: "black", borderWidth: 1 };
