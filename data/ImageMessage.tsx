65cfa70f07d09a9d1ddf6bc70cecebae7d1a788b
Christian Sturm <christian.sturm@code.berlin>
2024-10-28 19:00:15 +0100

    refac: extract JSX

--- data/ImageMessage.tsx
+++ data/ImageMessage.tsx
@@ -1,9 +1,3 @@
-import { Image } from "expo-image";
-import { ReactElement } from "react";
-import { Text, View } from "react-native";
-import { TestStyles } from "@/app/(tabs)/";
-const SAMPLE_IMAGE = require("@/assets/images/SampleImages/SampleImageMessage.png");
-
 // NOTE: OPTIMIZATION idea - Use .formatToParts() with only one formatter and pick the neccessary parts accordingly
 // NOTE: Using undefined for now until custom timezone is specified
 const DATE_FORMATTER = Intl.DateTimeFormat(undefined, {
@@ -17,30 +11,18 @@ const TIME_FORMATTER = Intl.DateTimeFormat(undefined, {
 });
 
 export abstract class ChatMessage {
-  #timestamp: Date;
+  private timestamp: Date;
 
-  dateString: string;
-  timeString: string;
+  public dateString: string;
+  public timeString: string;
 
   constructor(
     public author: string,
     timestamp: string,
   ) {
-    this.#timestamp = new Date(timestamp);
-    this.dateString = DATE_FORMATTER.format(this.#timestamp);
-    this.timeString = TIME_FORMATTER.format(this.#timestamp);
-  }
-
-  // TODO: Make a React Function Component that takes children-prop out of this:
-  // - Revert ChatMessage back to an interface
-  // - Create object with .MessageParent and .TextMessage & .ImageMessage
-  protected renderBaseMessage(body: ReactElement, includeAuthor: boolean) {
-    return (
-      <View style={TestStyles}>
-        {includeAuthor ? <Text>{this.author}</Text> : null}
-        {body}
-      </View>
-    );
+    this.timestamp = new Date(timestamp);
+    this.dateString = DATE_FORMATTER.format(this.timestamp);
+    this.timeString = TIME_FORMATTER.format(this.timestamp);
   }
 }
 
@@ -52,32 +34,14 @@ export class TextMessage extends ChatMessage {
   ) {
     super(author, timestamp);
   }
-
-  renderMessage(includeAuthor: boolean = true): ReactElement {
-    return super.renderBaseMessage(<Text>{this.text}</Text>, includeAuthor);
-  }
 }
 
 export class ImageMessage extends ChatMessage {
   constructor(
     author: string,
     timestamp: string,
-    /**
-     * Currenty Unused. Will eventually replace {@link SAMPLE_IMAGE}
-     */
     public imageURL: string,
   ) {
     super(author, timestamp);
-  }
-
-  renderMessage(includeAuthor: boolean = true): ReactElement {
-    return super.renderBaseMessage(
-      <Image
-        source={SAMPLE_IMAGE}
-        contentFit="scale-down"
-        style={{ width: "40%", height: 184 }}
-      />,
-      includeAuthor,
-    );
   }
 }
