def7ac759ac8b70c61168ec4cefa6ddddca20fb5
Christian Sturm <christian.sturm@code.berlin>
2024-10-28 18:21:39 +0100

    feat: add digit font and add Stylesheets where missing

--- constants/Fonts.ts
+++ constants/Fonts.ts
@@ -15,12 +15,12 @@ export const Fonts = {
 
     medium: { fontSize: 48, fontFamily: SPACE_MONO_REGULAR },
   }),
-  title: {
+  title: StyleSheet.create({
     h2: { fontSize: 48, lineHeight: 28, fontFamily: INTER_MEDIUM },
     h5: { fontSize: 28, lineHeight: 48, fontFamily: INTER_BOLD },
     h6: { fontSize: 28, lineHeight: 28, fontFamily: INTER_SEMIBOLD },
-  },
-  paragraph: {
+  }),
+  paragraph: StyleSheet.create({
     p1: { fontSize: 16, fontFamily: INTER_BOLD },
     p2: { fontSize: 16, fontFamily: INTER_SEMIBOLD },
     p4: { fontSize: 16, fontFamily: INTER_REGULAR },
@@ -30,5 +30,8 @@ export const Fonts = {
     p8: { fontSize: 12, fontFamily: INTER_REGULAR },
     p9: { fontSize: 12, fontFamily: INTER_MEDIUM },
     p10: { fontSize: 10, fontFamily: INTER_REGULAR },
-  },
+  }),
+  date: StyleSheet.create({
+    small: { fontSize: 8, fontFamily: INTER_REGULAR },
+  }),
 };
