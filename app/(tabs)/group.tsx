e28a5e610ea3d78d260a3bb25117861552f324f4
Christian Sturm <christian.sturm@code.berlin>
2024-10-28 22:15:03 +0100

    WIP

--- app/(tabs)/group.tsx
+++ app/(tabs)/group.tsx
@@ -5,37 +5,62 @@ import { FlashList } from "@shopify/flash-list";
 import { Image } from "expo-image";
 import { View, Text, StyleSheet } from "react-native";
 
+const MESSAGE_BORDER_RADIUS = 13;
 const CHAT_STYLES = StyleSheet.create({
-  chatContainer: { alignItems: "stretch" },
-  messageContainer: { backgroundColor: Colors.grey.light2 },
-  image: { borderRadius: 50, height: "25%" },
+  screenContainer: {
+    alignSelf: "center",
+    width: "90%",
+    height: "93%",
+  },
+  messageContainer: {
+    backgroundColor: Colors.grey.light2,
+    borderRadius: MESSAGE_BORDER_RADIUS,
+    paddingHorizontal: 12,
+    paddingVertical: 4,
+  },
+  messageText: { maxWidth: "80%" },
+  generalTimeText: {
+    color: Colors.grey.dark3,
+    alignSelf: "flex-end",
+  },
+  image: {
+    borderRadius: MESSAGE_BORDER_RADIUS,
+    width: 140,
+    height: 184,
+    alignSelf: "flex-start",
+  },
+  divider: { height: 12 },
 });
 
 export default function GroupScreen() {
+  function renderItem({ item, index }: { item: ChatDataItem; index: number }) {
+    function itemIsDateString(item: ChatDataItem): item is string {
+      return typeof item === "string";
+    }
+
+    function determinePreviousAuthor() {
+      let previousItem = SAMPLE_DATA_WITH_DATE[index - 1];
+      return typeof previousItem == "string" ? null : previousItem.author;
+    }
+
+    if (itemIsDateString(item)) {
+      return <Text>{item}</Text>;
+    } else {
+      return determineComponentByMessageType(
+        item,
+        item.author !== determinePreviousAuthor(),
+      );
+    }
+    //TODO: Check if last two entries should display "Today" or "Yesterday""
+  }
+
   return (
-    <View>
+    <View style={CHAT_STYLES.screenContainer}>
       <FlashList
-        style={CHAT_STYLES.chatContainer}
         data={SAMPLE_DATA_WITH_DATE}
-        renderItem={function ({ item, index }) {
-          function itemIsDateString(item: ChatDataItem): item is string {
-            return typeof item === "string";
-          }
-
-          function determinePreviousAuthor() {
-            let previousItem = SAMPLE_DATA_WITH_DATE[index - 1];
-            return typeof previousItem == "string" ? null : previousItem.author;
-          }
-
-          if (itemIsDateString(item)) {
-            return <Text>{item}</Text>;
-          } else {
-            return determineComponentByMessageType(
-              item,
-              item.author !== determinePreviousAuthor(),
-            );
-          }
-          //TODO: Check if last two entries should display "Today" or "Yesterday""
+        renderItem={renderItem}
+        ItemSeparatorComponent={function () {
+          return <View style={CHAT_STYLES.divider} />;
         }}
         estimatedItemSize={100}
         estimatedListSize={{ height: 592, width: 350 }}
@@ -53,10 +78,11 @@ export function determineComponentByMessageType(
       <TextMessageComponent shouldRenderAuthor={shouldShowAuthor} {...item} />
     );
   } else if (item instanceof ImageMessage) {
+    console.debug(item.imageURL);
     return (
       <Image
-        source={item.imageURL}
-        contentFit="scale-down"
+        source={{ uri: item.imageURL }}
+        contentFit="cover"
         style={CHAT_STYLES.image}
       />
     );
@@ -77,9 +103,13 @@ export function TextMessageComponent(props: {
         <Text style={Fonts.paragraph.p9}>{props.author}</Text>
       ) : null}
 
-      <Text style={Fonts.paragraph.p4}>{props.text}</Text>
+      <Text style={[Fonts.paragraph.p4, CHAT_STYLES.messageText]}>
+        {props.text}
+      </Text>
 
-      <Text style={Fonts.date.small}>{props.timeString}</Text>
+      <Text style={[Fonts.date.small, CHAT_STYLES.generalTimeText]}>
+        {props.timeString}
+      </Text>
     </View>
   );
 }
@@ -107,12 +137,7 @@ const SAMPLE_DATA = [
   new TextMessage(
     "Jung",
     "2024-08-11T17:03:10Z",
-    "cheer for your friend @user1!",
-  ),
-  new TextMessage(
-    "Jung",
-    "2024-08-11T17:03:10Z",
-    "cheer for your friend @user1!",
+    "We have a result from the last week’s challenge.\n@user1 and @user5 unfortunately didn’t meet the goal!\n\nPlease pay 10€ for the group fund.\nCurrent balance: 40€",
   ),
   new TextMessage(
     "Jung",
@@ -128,6 +153,11 @@ const SAMPLE_DATA = [
     "Jung",
     "2024-08-13T17:03:06Z",
     "cheer for your friend @user1!",
+  ),
+  new ImageMessage(
+    "Jung",
+    "2024-08-11T17:03:06Z",
+    "https://placehold.co/140x184",
   ),
 ];
 
