bc04f35541e4d8f57d0304ba9726b940175eb47b
Christian Sturm <christian.sturm@code.berlin>
2024-11-04 15:57:56 +0100

    feat: simplify chat message class

---
+++ data/DataAccessObjects/ChatMessageDAO.ts
@@ -0,0 +1,34 @@
+// NOTE: OPTIMIZATION idea - Use .formatToParts() with only one formatter and pick the neccessary parts accordingly
+//
+// NOTE: Using undefined for now until custom timezone is specified
+const DATE_FORMATTER = Intl.DateTimeFormat(undefined, {
+  year: "numeric",
+  month: "numeric",
+  day: "numeric",
+});
+const TIME_FORMATTER = Intl.DateTimeFormat(undefined, {
+  hour: "2-digit",
+  minute: "2-digit",
+});
+
+enum MessageType {
+  TEXT,
+  IMAGE,
+}
+
+export class TextMessageDAO {
+  public time: string;
+  public date: string;
+
+  constructor(
+    public messageId: string,
+    public author: string,
+    timestamp: string,
+    public content: string,
+    public type: MessageType,
+  ) {
+    const INSTANT = new Date(timestamp);
+    this.date = DATE_FORMATTER.format(INSTANT);
+    this.time = TIME_FORMATTER.format(INSTANT);
+  }
+}
