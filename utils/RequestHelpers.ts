c79e3506087c8e57cb2b50eb5b5ec73f17c70cc0
Christian Sturm <christian.sturm@code.berlin>
2024-12-03 21:50:22 +0100

    feat: factor out header as parameter and add function to build base header

--- utils/RequestHelpers.ts
+++ utils/RequestHelpers.ts
@@ -1,7 +1,9 @@
-import { RegistrationDetails } from "@/data/repository/UserRepository";
-import { typeCheckEnvVariable } from "./TypeHelpers";
+import UserRepository, {
+  RegistrationDetails,
+} from "@/data/repository/UserRepository";
 import { SafeDigits } from "./UtilityClasses";
 import { MessageDTO } from "@/data/DataTransferObjects/MessageDTO";
+import { typeCheckEnvVariable } from "./TypeHelpers";
 
 type apiPaths =
   | "registration"
@@ -13,6 +15,23 @@ type apiPaths =
 
 const API_BASE_ROUTE = "https://my.api.mockaroo.com";
 
+export async function buildBaseHeaders() {
+  const SESSION_ID = await UserRepository.findSessionId();
+  const HEADERS = new Headers();
+  HEADERS.append(
+    "X-API-Key",
+    typeCheckEnvVariable(
+      process.env.EXPO_PUBLIC_MOCKAROO_KEY,
+      "EXPO_PUBLIC_MOCKAROO_KEY",
+    ),
+  );
+
+  if (SESSION_ID !== null) {
+    HEADERS.append("Cookie", SESSION_ID);
+  }
+  return HEADERS;
+}
+
 /**
  * Builds a Request
  */
@@ -20,16 +39,12 @@ export function bulildRequest(
   route: apiPaths,
   method: "GET" | "POST" | "PUT" | "PATCH",
   queryParamPair: string,
+  headers: Headers,
   body?: RegistrationDetails | SafeDigits | MessageDTO | string,
 ) {
   return new Request(`${API_BASE_ROUTE}/${route}?${queryParamPair}`, {
-    method: method,
-    headers: {
-      "X-API-Key": typeCheckEnvVariable(
-        process.env.EXPO_PUBLIC_MOCKAROO_KEY,
-        "EXPO_PUBLIC_MOCKAROO_KEY",
-      ),
-    },
+    method,
+    headers,
     body:
       method === "GET"
         ? undefined
