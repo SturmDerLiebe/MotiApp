905567c1916be99fe123936062075c754e294866
Christian Sturm <christian.sturm@code.berlin>
2024-12-04 19:22:21 +0100

    feat: update UI and factor out ChatList

--- app/(tabs)/group.tsx
+++ app/(tabs)/group.tsx
@@ -1,70 +1,36 @@
+import { ChatList } from "@/components/chat/ChatList";
 import { CHAT_STYLES } from "@/components/chat/ChatStyles";
-import { MessageComponent } from "@/components/chat/MesssageComponent";
-import { Fonts } from "@/constants/Fonts";
-import useReceiveMessageState, {
-  ChatMessageListItem,
-} from "@/hooks/group/message/useReceiveMessages";
-import { FlashList } from "@shopify/flash-list";
-import { useEffect } from "react";
-import { View, Text } from "react-native";
+import { MessageType, NewChatMessage } from "@/data/DTO/ChatMessage";
+import useReceiveMessageState from "@/hooks/group/message/useReceiveMessages";
+import { useEffect, useState } from "react";
+import { TextInput, View } from "react-native";
 
 export default function GroupScreen() {
-  const [SOCKET_STATE, startReceivingMessages, stopReceivingMessages] =
-    useReceiveMessageState();
+  const [
+    socketState,
+    startReceivingMessages,
+    stopReceivingMessages,
+    sendNewMessage,
+  ] = useReceiveMessageState();
 
   useEffect(() => {
-    startReceivingMessages("12345"); // TODO: Save and Retreive GroupIdt:
+    startReceivingMessages();
     return stopReceivingMessages();
   }, [startReceivingMessages, stopReceivingMessages]);
 
+  const [message, setMessage] = useState("");
+
   return (
     <View style={CHAT_STYLES.screenContainer}>
-      <FlashList
-        data={SOCKET_STATE.mostRecentPayload}
-        renderItem={({ item, index }) => (
-          <ChatItem
-            item={item}
-            previousItem={SOCKET_STATE.mostRecentPayload[index - 1]}
-          />
-        )}
-        ItemSeparatorComponent={function () {
-          return <View style={CHAT_STYLES.divider} />;
+      <ChatList chatState={socketState} />
+      <TextInput
+        defaultValue={message}
+        onChange={(event) => setMessage(event.nativeEvent.text)}
+        onSubmitEditing={function sendMessage() {
+          sendNewMessage(new NewChatMessage(message, MessageType.TEXT));
         }}
-        estimatedItemSize={100}
-        estimatedListSize={{ height: 592, width: 350 }}
+        returnKeyType="send"
       />
     </View>
   );
-}
-
-function ChatItem({
-  item,
-  previousItem,
-}: {
-  item: ChatMessageListItem;
-  previousItem: ChatMessageListItem;
-}) {
-  //TODO: Check if last two entries should display "Today" or "Yesterday""
-  function itemIsDateString(item: ChatMessageListItem): item is string {
-    return typeof item === "string";
-  }
-
-  function determinePreviousAuthor() {
-    return typeof previousItem == "string" ? null : previousItem.author;
-  }
-
-  if (itemIsDateString(item)) {
-    function DateDivider({ date }: { date: string }) {
-      return <Text style={[Fonts.paragraph.p9, CHAT_STYLES.date]}>{date}</Text>;
-    }
-
-    return <DateDivider date={item} />;
-  } else {
-    return (
-      <MessageComponent
-        item={item}
-        previousAuthor={determinePreviousAuthor()}
-      />
-    );
-  }
 }
---
+++ components/chat/ChatList.tsx
@@ -0,0 +1,58 @@
+import { Fonts } from "@/constants/Fonts";
+import { ChatMessageListItem } from "@/hooks/group/message/useReceiveMessages";
+import { Text, View } from "react-native";
+import { CHAT_STYLES } from "./ChatStyles";
+import { MessageComponent } from "./MesssageComponent";
+import { FlashList } from "@shopify/flash-list";
+import { SocketStatus } from "@/utils/socket/status";
+
+export function ChatList({ chatState }: { chatState: SocketStatus }) {
+  return (
+    <FlashList
+      data={chatState.mostRecentPayload}
+      renderItem={({ item, index }) => (
+        <ChatItem
+          item={item}
+          previousItem={chatState.mostRecentPayload[index - 1]}
+        />
+      )}
+      ItemSeparatorComponent={function () {
+        return <View style={CHAT_STYLES.divider} />;
+      }}
+      estimatedItemSize={100}
+      estimatedListSize={{ height: 592, width: 350 }}
+    />
+  );
+}
+
+function ChatItem({
+  item,
+  previousItem,
+}: {
+  item: ChatMessageListItem;
+  previousItem: ChatMessageListItem;
+}) {
+  //TODO: Check if last two entries should display "Today" or "Yesterday""
+  function itemIsDateString(item: ChatMessageListItem): item is string {
+    return typeof item === "string";
+  }
+
+  function determinePreviousAuthor() {
+    return typeof previousItem == "string" ? null : previousItem.author;
+  }
+
+  if (itemIsDateString(item)) {
+    function DateDivider({ date }: { date: string }) {
+      return <Text style={[Fonts.paragraph.p9, CHAT_STYLES.date]}>{date}</Text>;
+    }
+
+    return <DateDivider date={item} />;
+  } else {
+    return (
+      <MessageComponent
+        item={item}
+        previousAuthor={determinePreviousAuthor()}
+      />
+    );
+  }
+}
