7ee2d8a73301f4b363093541ee72aa652248b62d
Christian Sturm <christian.sturm@code.berlin>
2024-12-12 22:58:47 +0100

    fix: swap log messages and format

--- hooks/group/message/useReceiveMessages.ts
+++ hooks/group/message/useReceiveMessages.ts
@@ -1,19 +1,19 @@
 import {
-  ChatMessage,
-  ExistingChatMessage,
-  NewChatMessage,
-  RawExistingMessageData,
-  transformRawToUIMessageList,
+    ChatMessage,
+    ExistingChatMessage,
+    NewChatMessage,
+    RawExistingMessageData,
+    transformRawToUIMessageList,
 } from "@/data/DTO/ChatMessage";
 import GroupRepository from "@/data/repository/GroupRepository";
 import { spliceChatMessageListWithDates } from "@/utils/ChatMessage/helper";
 import { Logger } from "@/utils/Logging/Logger";
 import {
-  SocketInitialLoading,
-  SocketStatus,
-  SocketListSuccess,
-  SocketError,
-  SocketListUpdateInitiated,
+    SocketInitialLoading,
+    SocketStatus,
+    SocketListSuccess,
+    SocketError,
+    SocketListUpdateInitiated,
 } from "@/utils/socket/status";
 import { useCallback, useRef, useState } from "react";
 
@@ -29,110 +29,118 @@ const TAG = "USE_RECEIVE_MESSAGES";
  * ```
  */
 export default function useReceiveMessageState(): [
-  SocketStatus,
-  () => void,
-  () => void,
-  (newMessage: NewChatMessage) => void,
+    SocketStatus,
+    () => void,
+    () => void,
+    (newMessage: NewChatMessage) => void,
 ] {
-  let [receivedMessageState, setReceivedMessageState] = useState<SocketStatus>(
-    new SocketInitialLoading(),
-  );
+    let [receivedMessageState, setReceivedMessageState] =
+        useState<SocketStatus>(new SocketInitialLoading());
 
-  const INTERVAL_REF = useRef<NodeJS.Timeout>();
+    const INTERVAL_REF = useRef<NodeJS.Timeout>();
 
-  return [
-    receivedMessageState,
-    useCallback(startSocketConnection, []),
-    useCallback(function stopReceivingMessages() {
-      Logger.logInfo(TAG, "Start Receiving Messages");
-      clearInterval(INTERVAL_REF.current);
-    }, []),
-    /**
-     * **Preformance Notice:**
-     * This function needs to create a new object for the `mostRecentPayload`, since {@link FlashList} will not rerender otherwise.
-     */
-    function mergeNewMessageInput(newMessage: NewChatMessage): void {
-      setReceivedMessageState((currentState) => {
-        return new SocketListUpdateInitiated([
-          ...currentState.mostRecentPayload,
-          newMessage,
-        ]);
-      });
-    },
-  ];
+    return [
+        receivedMessageState,
+        useCallback(startSocketConnection, []),
+        useCallback(function stopReceivingMessages() {
+            Logger.logInfo(TAG, "Cancel Receiving Messages");
+            clearInterval(INTERVAL_REF.current);
+        }, []),
+        /**
+         * **Preformance Notice:**
+         * This function needs to create a new object for the `mostRecentPayload`, since {@link FlashList} will not rerender otherwise.
+         */
+        function mergeNewMessageInput(newMessage: NewChatMessage): void {
+            setReceivedMessageState((currentState) => {
+                return new SocketListUpdateInitiated([
+                    ...currentState.mostRecentPayload,
+                    newMessage,
+                ]);
+            });
+        },
+    ];
 
-  async function startSocketConnection() {
-    Logger.logInfo(TAG, "Cancel Receiving Messages");
-    try {
-      handleResponse();
-    } catch (error) {
-      handleError(error);
-      clearInterval(INTERVAL_REF.current);
-    }
+    async function startSocketConnection() {
+        Logger.logInfo(TAG, "Start Receiving Messages");
+        try {
+            handleResponse();
+        } catch (error) {
+            handleError(error);
+            clearInterval(INTERVAL_REF.current);
+        }
 
-    /**
-     * @throws any {@link fetch} related Error
-     * @throws any {@link Response}.json related Error
-     */
-    async function handleResponse() {
-      const RESPONSE = await GroupRepository.receiveExistingMessages(); // TODO: This should arrive sorted from BE
+        /**
+         * @throws any {@link fetch} related Error
+         * @throws any {@link Response}.json related Error
+         */
+        async function handleResponse() {
+            const RESPONSE = await GroupRepository.receiveExistingMessages(); // TODO: This should arrive sorted from BE
 
-      //TODO: socket.on('listSuccess')
-      if (RESPONSE.ok) {
-        const RAW_MESSAGES: RawExistingMessageData[] = await RESPONSE.json();
-        const MESSAGES = transformRawToUIMessageList(RAW_MESSAGES);
+            //TODO: socket.on('listSuccess')
+            if (RESPONSE.ok) {
+                const RAW_MESSAGES: RawExistingMessageData[] =
+                    await RESPONSE.json();
+                const MESSAGES = transformRawToUIMessageList(RAW_MESSAGES);
 
-        const DATE_SPLICED_DATA = spliceChatMessageListWithDates(MESSAGES);
+                const DATE_SPLICED_DATA =
+                    spliceChatMessageListWithDates(MESSAGES);
 
-        setReceivedMessageState(new SocketListSuccess(DATE_SPLICED_DATA));
+                setReceivedMessageState(
+                    new SocketListSuccess(DATE_SPLICED_DATA),
+                );
 
-        startFetchingNewMessages();
-      } else {
-        //TODO: socket.on('error')
-        setReceivedMessageState(
-          (currentState) =>
-            new SocketError(1003, currentState.mostRecentPayload),
-          // SocketError.determineGeneralErrorMessage(RESPONSE.status, TAG),
-        );
-      }
+                startFetchingNewMessages();
+            } else {
+                //TODO: socket.on('error')
+                setReceivedMessageState(
+                    (currentState) =>
+                        new SocketError(1003, currentState.mostRecentPayload),
+                    // SocketError.determineGeneralErrorMessage(RESPONSE.status, TAG),
+                );
+            }
 
-      function startFetchingNewMessages() {
-        INTERVAL_REF.current = setInterval(async function () {
-          const RESPONSE = await GroupRepository.receiveNewMessages();
+            function startFetchingNewMessages() {
+                INTERVAL_REF.current = setInterval(async function () {
+                    const RESPONSE = await GroupRepository.receiveNewMessages();
 
-          //TODO: socket.on('listSuccess')
-          if (RESPONSE.ok) {
-            //NOTE: Our Mock API returns a non Array JSON Response for single element Requests. This needs to be changed with the real implementation.
-            const DATA: RawExistingMessageData = await RESPONSE.json();
+                    //TODO: socket.on('listSuccess')
+                    if (RESPONSE.ok) {
+                        //NOTE: Our Mock API returns a non Array JSON Response for single element Requests. This needs to be changed with the real implementation.
+                        const DATA: RawExistingMessageData =
+                            await RESPONSE.json();
 
-            setReceivedMessageState(
-              ({ mostRecentPayload }) =>
-                new SocketListSuccess([
-                  ...mostRecentPayload,
-                  new ExistingChatMessage(DATA),
-                ]),
+                        setReceivedMessageState(
+                            ({ mostRecentPayload }) =>
+                                new SocketListSuccess([
+                                    ...mostRecentPayload,
+                                    new ExistingChatMessage(DATA),
+                                ]),
+                        );
+                    } else {
+                        //TODO: socket.on('error')
+                        setReceivedMessageState(
+                            (currentState) =>
+                                new SocketError(
+                                    1003,
+                                    currentState.mostRecentPayload,
+                                ),
+                            // SocketError.determineGeneralErrorMessage(RESPONSE.status, TAG),
+                        );
+                    }
+                }, 5000);
+            }
+        }
+
+        function handleError(error: unknown) {
+            console.error(
+                TAG,
+                "There was an Error during the connection to a socket:",
+                error,
             );
-          } else {
-            //TODO: socket.on('error')
             setReceivedMessageState(
-              (currentState) =>
-                new SocketError(1003, currentState.mostRecentPayload),
-              // SocketError.determineGeneralErrorMessage(RESPONSE.status, TAG),
+                (currentState) =>
+                    new SocketError(1008, currentState.mostRecentPayload),
             );
-          }
-        }, 5000);
-      }
+        }
     }
-
-    function handleError(error: unknown) {
-      console.error(
-        TAG,
-        "There was an Error during the connection to a socket:",
-        error,
-      );
-      setReceivedMessageState(
-        (currentState) => new SocketError(1008, currentState.mostRecentPayload),
-      );
-    }
-  }
 }
