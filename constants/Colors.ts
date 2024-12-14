b1e094e1b1fcb3542c19601ddbf447c23a238622
Christian Sturm <christian.sturm@code.berlin>
2024-12-14 20:37:42 +0100

    format

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,26 +1,26 @@
 export const Colors = {
-  grey: {
-    light1: "#F3F4F5",
-    light2: "#F0F1F3",
-    light3: "#E5E7E9",
-    dark1: "#D0D5DD",
-    dark2: "#9EA8B3",
-    dark3: "#737C8D",
-  },
-  blue: {
-    dark: "#1D3D5E",
-    grey: "#142132",
-  },
-  white: "#FFFFFF",
-  transparent: "#FFFFFF00",
-  red: "#D12E34",
-  green: "#317D35",
-  eggplant: {
-    light: "#DAC8D3",
-    dark: "#795069",
-  },
-  orange: {
-    light: "#F9E0C9",
-    dark: "#EA974C",
-  },
+    grey: {
+        light1: "#F3F4F5",
+        light2: "#F0F1F3",
+        light3: "#E5E7E9",
+        dark1: "#D0D5DD",
+        dark2: "#9EA8B3",
+        dark3: "#737C8D",
+    },
+    blue: {
+        dark: "#1D3D5E",
+        grey: "#142132",
+    },
+    white: "#FFFFFF",
+    transparent: "#FFFFFF00",
+    red: "#D12E34",
+    green: "#317D35",
+    eggplant: {
+        light: "#DAC8D3",
+        dark: "#795069",
+    },
+    orange: {
+        light: "#F9E0C9",
+        dark: "#EA974C",
+    },
 };
