b8f10b508a6bfa36c4cfe1cca61066cee9fcc0c8
Christian Sturm <christian.sturm@code.berlin>
2024-12-15 20:44:50 +0100

    feat: extend logger with error logs

--- utils/Logging/Logger.ts
+++ utils/Logging/Logger.ts
@@ -2,4 +2,9 @@ export const Logger = {
     logInfo(tag: string, message: string) {
         console.info(`${tag} |====> ${message}`);
     },
+
+    logError(tag: string, message: string, error: Error) {
+        console.error(`${tag} |====> ${message}
+Original Error: ${error}`);
+    },
 };
