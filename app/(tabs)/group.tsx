86915b92cfdeb26bdbc17f7882d8f84203154bb9
Christian Sturm <christian.sturm@code.berlin>
2024-12-15 13:56:10 +0100

    format

--- app/(tabs)/group.tsx
+++ app/(tabs)/group.tsx
@@ -6,36 +6,38 @@ import { useEffect, useRef, useState } from "react";
 import { StyleSheet, TextInput, View } from "react-native";
 
 export default function GroupScreen() {
-  const TEXT_INPUT_REF = useRef<TextInput>(null);
+    const TEXT_INPUT_REF = useRef<TextInput>(null);
 
-  const [
-    socketState,
-    startReceivingMessages,
-    stopReceivingMessages,
-    sendNewMessage,
-  ] = useReceiveMessageState();
+    const [
+        socketState,
+        startReceivingMessages,
+        stopReceivingMessages,
+        sendNewMessage,
+    ] = useReceiveMessageState();
 
-  useEffect(() => {
-    startReceivingMessages();
-    return stopReceivingMessages;
-  }, [startReceivingMessages, stopReceivingMessages]);
+    useEffect(() => {
+        startReceivingMessages();
+        return stopReceivingMessages;
+    }, [startReceivingMessages, stopReceivingMessages]);
 
-  const [message, setMessage] = useState("");
+    const [message, setMessage] = useState("");
 
-  return (
-    <View style={CHAT_STYLES.screenContainer}>
-      <ChatList chatState={socketState} />
-      <TextInput
-        ref={TEXT_INPUT_REF}
-        defaultValue={message}
-        onChange={(event) => setMessage(event.nativeEvent.text)}
-        onSubmitEditing={function sendMessage() {
-          sendNewMessage(new NewChatMessage(message, MessageType.TEXT));
-          TEXT_INPUT_REF.current?.clear();
-        }}
-        returnKeyType="send"
-        style={{ borderWidth: StyleSheet.hairlineWidth }}
-      />
-    </View>
-  );
+    return (
+        <View style={CHAT_STYLES.screenContainer}>
+            <ChatList chatState={socketState} />
+            <TextInput
+                ref={TEXT_INPUT_REF}
+                defaultValue={message}
+                onChange={(event) => setMessage(event.nativeEvent.text)}
+                onSubmitEditing={function sendMessage() {
+                    sendNewMessage(
+                        new NewChatMessage(message, MessageType.TEXT),
+                    );
+                    TEXT_INPUT_REF.current?.clear();
+                }}
+                returnKeyType="send"
+                style={{ borderWidth: StyleSheet.hairlineWidth }}
+            />
+        </View>
+    );
 }
