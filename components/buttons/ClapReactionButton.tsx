e96d22dfc85c678389db5291707152d1d14e9794
Christian Sturm <christian.sturm@code.berlin>
2024-12-16 21:14:21 +0100

    format

--- components/buttons/ClapReactionButton.tsx
+++ components/buttons/ClapReactionButton.tsx
@@ -4,65 +4,65 @@ import { useState } from "react";
 import { Pressable, Text, View } from "react-native";
 
 export function ClapReactionButton({
-  initialCount = 0,
+    initialCount = 0,
 }: {
-  initialCount: number;
+    initialCount: number;
 }) {
-  function hasAnyReactions(amount: number) {
-    return amount > 0;
-  }
-  const [count, setCount] = useState(initialCount);
+    function hasAnyReactions(amount: number) {
+        return amount > 0;
+    }
+    const [count, setCount] = useState(initialCount);
 
-  return (
-    <Pressable
-      onPress={function () {
-        setCount((current) =>
-          !hasAnyReactions(current) ? current + 1 : current - 1,
-        );
-      }}
-      style={{ flexDirection: "row", alignItems: "center" }}
-    >
-      <ClapReactionIcon anyReactionsYet={hasAnyReactions(count)} />
-      <Text style={[Fonts.paragraph.p9, { paddingStart: 4 }]}>
-        {hasAnyReactions(count) ? count : null}
-      </Text>
-    </Pressable>
-  );
+    return (
+        <Pressable
+            onPress={function () {
+                setCount((current) =>
+                    !hasAnyReactions(current) ? current + 1 : current - 1,
+                );
+            }}
+            style={{ flexDirection: "row", alignItems: "center" }}
+        >
+            <ClapReactionIcon anyReactionsYet={hasAnyReactions(count)} />
+            <Text style={[Fonts.paragraph.p9, { paddingStart: 4 }]}>
+                {hasAnyReactions(count) ? count : null}
+            </Text>
+        </Pressable>
+    );
 }
 
 const CLAP_EMOJI = "👏";
 
 function ClapReactionIcon(props: { anyReactionsYet: boolean }) {
-  return (
-    <View
-      style={{
-        alignSelf: "center",
-        width: 30,
-        aspectRatio: 1,
+    return (
+        <View
+            style={{
+                alignSelf: "center",
+                width: 30,
+                aspectRatio: 1,
 
-        backgroundColor: Colors.white,
-        borderRadius: 100,
+                backgroundColor: Colors.white,
+                borderRadius: 100,
 
-        elevation: 3,
-        shadowOffset: { width: 0, height: 2.27 },
-        shadowColor: "#8D8D8F",
-        shadowOpacity: 0.2,
+                elevation: 3,
+                shadowOffset: { width: 0, height: 2.27 },
+                shadowColor: "#8D8D8F",
+                shadowOpacity: 0.2,
 
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      <Text
-        style={{
-          fontSize: 16,
-          textAlign: "center",
-          textAlignVertical: "center",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <Text
+                style={{
+                    fontSize: 16,
+                    textAlign: "center",
+                    textAlignVertical: "center",
 
-          opacity: props.anyReactionsYet ? 1 : 0.2,
-        }}
-      >
-        {CLAP_EMOJI}
-      </Text>
-    </View>
-  );
+                    opacity: props.anyReactionsYet ? 1 : 0.2,
+                }}
+            >
+                {CLAP_EMOJI}
+            </Text>
+        </View>
+    );
 }
