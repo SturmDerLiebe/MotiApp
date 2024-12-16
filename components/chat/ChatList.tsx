15eb2fde826d29b6c336151a0af0bb07ad801bab
Christian Sturm <christian.sturm@code.berlin>
2024-12-16 13:16:23 +0100

    format

--- components/chat/ChatList.tsx
+++ components/chat/ChatList.tsx
@@ -9,65 +9,72 @@ import { CHAT_STYLES } from "./ChatStyles";
 import { MessageComponent } from "./MesssageComponent";
 
 export function ChatList({
-  chatState: { mostRecentPayload },
+    chatState: { mostRecentPayload },
 }: {
-  chatState: SocketStatus;
+    chatState: SocketStatus;
 }) {
-  const FLASH_LIST_REF = useRef<FlashList<ChatMessageListItem>>(null);
+    const FLASH_LIST_REF = useRef<FlashList<ChatMessageListItem>>(null);
 
-  return (
-    <FlashList
-      ref={FLASH_LIST_REF}
-      data={mostRecentPayload}
-      renderItem={({ item, index }) => (
-        <ChatItem item={item} previousItem={mostRecentPayload[index - 1]} />
-      )}
-      ItemSeparatorComponent={function () {
-        return <View style={CHAT_STYLES.divider} />;
-      }}
-      estimatedItemSize={100}
-      estimatedListSize={{ height: 592, width: 350 }}
-      initialScrollIndex={mostRecentPayload.length - 1}
-      onContentSizeChange={(_, height) =>
-        FLASH_LIST_REF.current?.scrollToOffset({ offset: height })
-      }
-      getItemType={(item) => {
-        return typeof item === "string" ? 100 : item.type;
-      }}
-    />
-  );
+    return (
+        <FlashList
+            ref={FLASH_LIST_REF}
+            data={mostRecentPayload}
+            renderItem={({ item, index }) => (
+                <ChatItem
+                    item={item}
+                    previousItem={mostRecentPayload[index - 1]}
+                />
+            )}
+            ItemSeparatorComponent={function () {
+                return <View style={CHAT_STYLES.divider} />;
+            }}
+            estimatedItemSize={100}
+            estimatedListSize={{ height: 592, width: 350 }}
+            initialScrollIndex={mostRecentPayload.length - 1}
+            onContentSizeChange={(_, height) =>
+                FLASH_LIST_REF.current?.scrollToOffset({ offset: height })
+            }
+            getItemType={(item) => {
+                return typeof item === "string" ? 100 : item.type;
+            }}
+        />
+    );
 }
 
 function ChatItem({
-  item,
-  previousItem,
+    item,
+    previousItem,
 }: {
-  item: ChatMessageListItem;
-  previousItem: ChatMessageListItem;
+    item: ChatMessageListItem;
+    previousItem: ChatMessageListItem;
 }) {
-  //TODO: Check if last two entries should display "Today" or "Yesterday""
-  function itemIsDateString(item: ChatMessageListItem): item is string {
-    return typeof item === "string";
-  }
-
-  function determinePreviousAuthor() {
-    return previousItem instanceof ExistingChatMessage
-      ? previousItem.author
-      : null;
-  }
-
-  if (itemIsDateString(item)) {
-    function DateDivider({ date }: { date: string }) {
-      return <Text style={[Fonts.paragraph.p9, CHAT_STYLES.date]}>{date}</Text>;
+    //TODO: Check if last two entries should display "Today" or "Yesterday""
+    function itemIsDateString(item: ChatMessageListItem): item is string {
+        return typeof item === "string";
     }
 
-    return <DateDivider date={item} />;
-  } else {
-    return (
-      <MessageComponent
-        item={item}
-        previousAuthor={determinePreviousAuthor()}
-      />
-    );
-  }
+    function determinePreviousAuthor() {
+        return previousItem instanceof ExistingChatMessage
+            ? previousItem.author
+            : null;
+    }
+
+    if (itemIsDateString(item)) {
+        function DateDivider({ date }: { date: string }) {
+            return (
+                <Text style={[Fonts.paragraph.p9, CHAT_STYLES.date]}>
+                    {date}
+                </Text>
+            );
+        }
+
+        return <DateDivider date={item} />;
+    } else {
+        return (
+            <MessageComponent
+                item={item}
+                previousAuthor={determinePreviousAuthor()}
+            />
+        );
+    }
 }
