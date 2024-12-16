89df2babd82a45b6b94ddf1ef7f2c6d7827addba
Christian Sturm <christian.sturm@code.berlin>
2024-12-16 20:52:35 +0100

    format

--- components/chat/ChatStyles.ts
+++ components/chat/ChatStyles.ts
@@ -3,58 +3,58 @@ import { StyleSheet } from "react-native";
 
 const MESSAGE_BORDER_RADIUS = 10;
 export const CHAT_STYLES = StyleSheet.create({
-  screenContainer: {
-    alignSelf: "center",
-    width: "90%",
-    height: "93%",
-  },
-  messageContainer: {
-    flexDirection: "row",
-  },
-  avatar: {
-    alignSelf: "flex-end",
-    borderRadius: 100,
-    width: 30,
-    aspectRatio: 1,
-  },
-  bodyBase: {
-    flex: 1,
-    backgroundColor: Colors.grey.light2,
-    borderRadius: MESSAGE_BORDER_RADIUS,
-    paddingTop: 4,
-    paddingStart: 12,
-  },
-  headerAndMainContainer: {
-    paddingBottom: 8,
-  },
-  time: {
-    position: "absolute",
-    end: 8,
-    bottom: 4,
+    screenContainer: {
+        alignSelf: "center",
+        width: "90%",
+        height: "93%",
+    },
+    messageContainer: {
+        flexDirection: "row",
+    },
+    avatar: {
+        alignSelf: "flex-end",
+        borderRadius: 100,
+        width: 30,
+        aspectRatio: 1,
+    },
+    bodyBase: {
+        flex: 1,
+        backgroundColor: Colors.grey.light2,
+        borderRadius: MESSAGE_BORDER_RADIUS,
+        paddingTop: 4,
+        paddingStart: 12,
+    },
+    headerAndMainContainer: {
+        paddingBottom: 8,
+    },
+    time: {
+        position: "absolute",
+        end: 8,
+        bottom: 4,
 
-    color: Colors.grey.dark3,
-  },
-  clapReactionContainer: {
-    width: "15%",
-    justifyContent: "center",
-    paddingStart: 10,
-  },
-  image: {
-    borderRadius: MESSAGE_BORDER_RADIUS,
-    width: 140,
-    height: 184,
-    alignSelf: "flex-start",
-  },
-  date: {
-    alignSelf: "center",
-    textAlign: "center",
+        color: Colors.grey.dark3,
+    },
+    clapReactionContainer: {
+        width: "15%",
+        justifyContent: "center",
+        paddingStart: 10,
+    },
+    image: {
+        borderRadius: MESSAGE_BORDER_RADIUS,
+        width: 140,
+        height: 184,
+        alignSelf: "flex-start",
+    },
+    date: {
+        alignSelf: "center",
+        textAlign: "center",
 
-    backgroundColor: Colors.grey.dark3,
-    color: Colors.white,
+        backgroundColor: Colors.grey.dark3,
+        color: Colors.white,
 
-    borderRadius: 100,
-    paddingVertical: 2,
-    paddingHorizontal: 8,
-  },
-  divider: { height: 12 },
+        borderRadius: 100,
+        paddingVertical: 2,
+        paddingHorizontal: 8,
+    },
+    divider: { height: 12 },
 });
