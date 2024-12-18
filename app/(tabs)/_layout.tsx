15da6ec6c8032f75aed278be815130330546764d
Christian Sturm <christian.sturm@code.berlin>
2024-12-18 22:43:40 +0100

    format

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -41,6 +41,7 @@ export default function TabContextWrapper() {
 function TabLayout() {
     NavigationBar.setBackgroundColorAsync(TabBarStyles.backgroundColor);
     const userInfoState = useUserInfoContext();
+
     const [requestUserInfo, cancelUserInfoRequest] =
         useControlUserInfoContext();
 
