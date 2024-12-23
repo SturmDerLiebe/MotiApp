be671b62b641df333e05f0d8c38c3b445cd662ef
Christian Sturm <christian.sturm@code.berlin>
2024-12-23 13:58:09 +0100

    feat: add merge function to Button helpers

--- components/buttons/helper.ts
+++ components/buttons/helper.ts
@@ -1,10 +1,10 @@
-import { StyleProp, ViewStyle } from "react-native";
-import { type BUTTON_STYLES } from "./styles";
+import { StyleProp, StyleSheet, ViewStyle } from "react-native";
+import { BASIC_BUTTON_STYLE, BUTTON_STYLES } from "./styles";
 
 /**
  * Picks out the Styles Object matching the current Button State
  */
-export function determineButtonStyleNoBorder(
+function determineButtonStyleNoBorder(
     buttonTypeStyles: (typeof BUTTON_STYLES)[keyof typeof BUTTON_STYLES],
     pressed: boolean,
     disabled: boolean,
@@ -16,4 +16,19 @@ export function determineButtonStyleNoBorder(
     } else {
         return buttonTypeStyles.inactive;
     }
+}
+
+/**
+ * Merges Basic and extra Styles with the Style derived from the Button State i.e. `pressed` and `disabled`
+ */
+export function mergeButtonStyles(
+    individualStyles: (typeof BUTTON_STYLES)[keyof typeof BUTTON_STYLES],
+    { pressed, disabled }: { pressed: boolean; disabled: boolean },
+    extraStyles: StyleProp<ViewStyle>,
+): StyleProp<ViewStyle> {
+    return StyleSheet.flatten([
+        BASIC_BUTTON_STYLE,
+        determineButtonStyleNoBorder(individualStyles, pressed, disabled),
+        extraStyles,
+    ]);
 }
