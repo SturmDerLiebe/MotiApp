23b64c8e65e1510415e212752631018cc4034936
Christian Sturm <christian.sturm@code.berlin>
2024-12-23 12:52:31 +0100

    feat: refactor and expand Button styles for DangerButtons

---
+++ components/buttons/styles.ts
@@ -0,0 +1,29 @@
+import { Colors } from "@/constants/Colors";
+import { Fonts } from "@/constants/Fonts";
+import { StyleProp, StyleSheet, TextStyle, ViewStyle } from "react-native";
+
+export const BUTTON_TEXT_STYLE: StyleProp<TextStyle> = {
+    color: Colors.white,
+    ...Fonts.paragraph.p2,
+    paddingVertical: 12,
+    textAlign: "center",
+};
+
+export const BASIC_BUTTON_STYLE: StyleProp<ViewStyle> = { borderRadius: 100 };
+
+export const BUTTON_STYLES = {
+    primary: StyleSheet.create({
+        inactive: { backgroundColor: Colors.blue.grey },
+        active: { backgroundColor: Colors.blue.dark },
+        disabled: { backgroundColor: Colors.grey.dark1 },
+    }),
+    danger: StyleSheet.create({
+        inactive: { backgroundColor: Colors.red.medium },
+        active: {
+            backgroundColor: Colors.blue.dark,
+            borderWidth: 1,
+            borderColor: Colors.red.medium,
+        },
+        disabled: { backgroundColor: Colors.red.light },
+    }),
+};
