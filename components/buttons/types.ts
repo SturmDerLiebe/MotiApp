3d7d9d26431756d74b6dccea871f2a3aafbd9b60
Christian Sturm <christian.sturm@code.berlin>
2024-12-25 15:49:01 +0100

    fix: demand disabled prop and introduce re add icon prop

--- components/buttons/types.ts
+++ components/buttons/types.ts
@@ -1,7 +1,10 @@
+import { Icons } from "@/constants/Icons";
 import { PressableProps, StyleProp, ViewStyle } from "react-native";
 
-export type ButtonProps = Pick<PressableProps, "disabled" | "onPress"> & {
+export type ButtonProps = Pick<PressableProps, "onPress"> & {
     /** A Label the button should display inside of it */
     title: string;
+    disabled: boolean;
     style?: StyleProp<ViewStyle>;
+    icon?: keyof typeof Icons;
 };
