6794270239b1e010de7d4abad9a124c5690c1ec2
Christian Sturm <christian.sturm@code.berlin>
2024-12-25 14:36:21 +0100

    fix: make styles optional

--- components/buttons/types.ts
+++ components/buttons/types.ts
@@ -3,5 +3,5 @@ import { PressableProps, StyleProp, ViewStyle } from "react-native";
 export type ButtonProps = Pick<PressableProps, "disabled" | "onPress"> & {
     /** A Label the button should display inside of it */
     title: string;
-    style: StyleProp<ViewStyle>;
+    style?: StyleProp<ViewStyle>;
 };
