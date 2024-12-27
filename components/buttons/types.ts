220b4d4b01fc44d1c8125b92d18e808112bbeff9
Christian Sturm <christian.sturm@code.berlin>
2024-12-27 19:43:09 +0100

    feat: extend styles

--- components/buttons/types.ts
+++ components/buttons/types.ts
@@ -1,10 +1,11 @@
 import { Icons } from "@/constants/Icons";
-import { PressableProps, StyleProp, ViewStyle } from "react-native";
+import { PressableProps, StyleProp, TextStyle, ViewStyle } from "react-native";
 
 export type ButtonProps = Pick<PressableProps, "onPress"> & {
     /** A Label the button should display inside of it */
     title: string;
-    disabled: boolean;
-    style?: StyleProp<ViewStyle>;
+    disabled?: boolean;
+    buttonStyle?: StyleProp<ViewStyle>;
+    textStyle?: StyleProp<TextStyle>;
     icon?: keyof typeof Icons;
 };
