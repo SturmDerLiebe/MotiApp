7c52bb2940d975984df6cd31fbe6b59bc7ba8237
Christian Sturm <christian.sturm@code.berlin>
2024-12-28 19:55:14 +0100

    feat: extend Buttons with separate text and button styles and add optional icons

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -10,12 +10,14 @@ import {
 import { mergeButtonStyles } from "./buttons/helper";
 import { BUTTON_STYLES, BUTTON_TEXT_STYLE } from "./buttons/styles";
 import type { ButtonProps } from "./buttons/types";
+import { Icon } from "@/constants/Icons";
 
 export function PrimaryButton({
     title,
-    disabled,
+    disabled = false,
     onPress,
-    style,
+    buttonStyle,
+    textStyle,
 }: ButtonProps) {
     return (
         <Pressable
@@ -24,26 +26,23 @@ export function PrimaryButton({
                 return mergeButtonStyles(
                     BUTTON_STYLES.primary,
                     { pressed, disabled: disabled ?? false },
-                    style,
+                    buttonStyle,
                 );
             }}
             onPress={onPress}
         >
-            <Text style={BUTTON_TEXT_STYLE}>{title}</Text>
+            <Text style={[BUTTON_TEXT_STYLE, textStyle]}>{title}</Text>
         </Pressable>
     );
 }
 
 export function SecondaryButton({
-    disabled,
+    disabled = false,
     title,
-    style,
+    buttonStyle,
+    textStyle,
     onPress,
-}: PressableProps & {
-    /** A Label the button shoud display inside of it */
-    title: string;
-    style?: StyleProp<ViewStyle>;
-}) {
+}: ButtonProps) {
     return (
         <Pressable
             disabled={disabled}
@@ -52,7 +51,7 @@ export function SecondaryButton({
                     mergeButtonStyles(
                         BUTTON_STYLES.secondary,
                         { pressed, disabled: disabled ?? false },
-                        style,
+                        buttonStyle,
                     ),
                     { borderWidth: 1.5 },
                 );
@@ -63,6 +62,7 @@ export function SecondaryButton({
                 style={[
                     BUTTON_TEXT_STYLE,
                     { color: disabled ? Colors.grey.dark1 : Colors.blue.grey },
+                    textStyle,
                 ]}
             >
                 {title}
@@ -71,23 +71,43 @@ export function SecondaryButton({
     );
 }
 
-export function DangerButton({ title, disabled, onPress, style }: ButtonProps) {
+export function DangerButton({
+    title,
+    disabled = false,
+    onPress,
+    buttonStyle,
+    textStyle,
+    icon,
+}: ButtonProps) {
     return (
         <Pressable
             disabled={disabled}
             style={({ pressed }) => {
-                return mergeButtonStyles(
-                    BUTTON_STYLES.danger,
-                    {
-                        pressed,
-                        disabled: disabled ?? false,
-                    },
-                    style,
+                return StyleSheet.compose(
+                    mergeButtonStyles(
+                        BUTTON_STYLES.danger,
+                        {
+                            pressed,
+                            disabled: disabled ?? false,
+                        },
+                        buttonStyle,
+                    ),
+                    icon !== undefined
+                        ? {
+                              flexDirection: "row",
+                              gap: 8,
+                              justifyContent: "center",
+                          }
+                        : null,
                 );
             }}
             onPress={onPress}
         >
-            <Text style={BUTTON_TEXT_STYLE}>{title}</Text>
+            {icon !== undefined ? (
+                <Icon icon={icon} tintColor={Colors.white} size={20} />
+            ) : null}
+
+            <Text style={[BUTTON_TEXT_STYLE, textStyle]}>{title}</Text>
         </Pressable>
     );
 }
