7f699213a5213767bdc6e32e43d134f150e6bf5d
Christian Sturm <christian.sturm@code.berlin>
2024-12-31 01:37:16 +0100

    feat: add SplashScreen Asset

--- constants/Icons.tsx
+++ constants/Icons.tsx
@@ -36,3 +36,7 @@ export const Icons = {
 export const MockIcons = {
     Circle: require("@/assets/images/mock_svg/Circle.svg"),
 };
+
+export const Assets = {
+    SplashIcon: require("@/assets/images/Icon.png"),
+};
