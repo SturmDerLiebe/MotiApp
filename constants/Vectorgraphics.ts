666deebc37e2ae0f1185222666d55fb120770c91
Christian Sturm <christian.sturm@code.berlin>
2025-01-02 00:59:54 +0100

    feat: add new vector graphics

---
+++ constants/Vectorgraphics.ts
@@ -0,0 +1,7 @@
+export type VectorNames = keyof typeof Vectorgraphics;
+
+export const Vectorgraphics = {
+    StretchingPerson: require("@/assets/images/optimized_svg/Illustrations/StretchingPerson.svg"),
+    TriumphCupPeople: require("@/assets/images/optimized_svg/Illustrations/TriumphCupPeople.svg"),
+    JumpingRopePerson: require("@/assets/images/optimized_svg/Illustrations/JumpingRopePerson.svg"),
+};
