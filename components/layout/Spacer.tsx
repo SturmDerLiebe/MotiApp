f085fe2382580dfa76d1326997c2a8928149eb95
Christian Sturm <christian.sturm@code.berlin>
2025-01-02 01:01:33 +0100

    feat: add flex prop to Horizontal Spacer

--- components/layout/Spacer.tsx
+++ components/layout/Spacer.tsx
@@ -1,5 +1,22 @@
 import { DimensionValue, View } from "react-native";
 
-export function HorizontalSpacer({ height }: { height: DimensionValue }) {
-    return <View style={{ height: height }} />;
+/**
+ * A Horizontal Spacer getting its height from a flex number
+ */
+export function HorizontalSpacer(props: { flex: number }): React.JSX.Element;
+/**
+ * A Horizontal Spacer getting its height from a {@link DimensionValue}
+ */
+export function HorizontalSpacer(props: {
+    height: DimensionValue;
+}): React.JSX.Element;
+export function HorizontalSpacer(props: {
+    height?: DimensionValue;
+    flex?: number;
+}): React.JSX.Element {
+    return props.flex !== undefined ? (
+        <View style={{ flex: props.flex }} />
+    ) : (
+        <View style={{ height: props.height }} />
+    );
 }
