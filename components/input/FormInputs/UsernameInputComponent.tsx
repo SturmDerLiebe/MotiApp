9c6488e37a615f8889422ddc5e0757f591514a78
Christian Sturm <christian.sturm@code.berlin>
2025-01-24 18:44:22 +0100

    feat: add new Username Input in preparation for refactoring

---
+++ components/input/FormInputs/UsernameInputComponent.tsx
@@ -0,0 +1,43 @@
+import { TextInput } from "react-native-gesture-handler";
+import { BaseInputComponent } from "./BaseInputComponent";
+import { forwardRef } from "react";
+import type { RegistrationFormAction } from "@/hooks/reducer/registrationReducer";
+
+export const UsernameInputComponent = forwardRef<
+    TextInput,
+    {
+        isValid: boolean | null;
+        dispatchRegistrationFormAction: React.Dispatch<RegistrationFormAction>;
+        focusNext: () => void;
+    }
+>(function UsernameInputComponent(
+    { isValid, dispatchRegistrationFormAction, focusNext },
+    ref,
+) {
+    return (
+        <BaseInputComponent
+            label="Username (Nickname)"
+            isValid={isValid ?? true}
+            hint="e.g. Jane"
+            keyboardType="default"
+            autoComplete="username"
+            returnKeyType="next"
+            placeholder="How should we call you?"
+            onChangeText={(newInput) =>
+                dispatchRegistrationFormAction({
+                    type: "UsernameEdit",
+                    payload: newInput,
+                })
+            }
+            onSubmitEditing={focusNext}
+            onEndEditing={function ({ nativeEvent: { text } }) {
+                const REGEX = /^\p{L}{1,25}(?:\s\p{L}{1,25})?$/u;
+                dispatchRegistrationFormAction({
+                    type: "UsernameValidate",
+                    payload: REGEX.test(text),
+                });
+            }}
+            ref={ref}
+        />
+    );
+});
