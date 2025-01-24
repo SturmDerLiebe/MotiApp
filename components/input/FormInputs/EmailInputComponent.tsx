ac053e9da4079eb49896f6ee7cf34d6991e8125c
Christian Sturm <christian.sturm@code.berlin>
2025-01-24 18:50:08 +0100

    feat: add new Email input for Refac

---
+++ components/input/FormInputs/EmailInputComponent.tsx
@@ -0,0 +1,43 @@
+import { TextInput } from "react-native-gesture-handler";
+import { BaseInputComponent } from "./BaseInputComponent";
+import { forwardRef } from "react";
+import type { RegistrationFormAction } from "@/hooks/reducer/registrationReducer";
+
+export const EmailInputComponent = forwardRef<
+    TextInput,
+    {
+        isValid: boolean | null;
+        dispatchRegistrationFormAction: React.Dispatch<RegistrationFormAction>;
+        focusNext: () => void;
+    }
+>(function EmailInputComponent(
+    { isValid, dispatchRegistrationFormAction, focusNext },
+    ref,
+) {
+    return (
+        <BaseInputComponent
+            label="E-Mail"
+            isValid={isValid ?? true}
+            hint="e.g. jane-doe.uk@gmail.com"
+            keyboardType="email-address"
+            autoComplete="email"
+            returnKeyType="next"
+            placeholder="Enter your Email"
+            onChangeText={(newInput) =>
+                dispatchRegistrationFormAction({
+                    type: "EmailEdit",
+                    payload: newInput,
+                })
+            }
+            onSubmitEditing={focusNext}
+            onEndEditing={function ({ nativeEvent: { text } }) {
+                const REGEX = /^[\w\-\.]{1,63}@[\w-]{1,63}\.[\w]{2,63}$/;
+                dispatchRegistrationFormAction({
+                    type: "EmailValidate",
+                    payload: REGEX.test(text),
+                });
+            }}
+            ref={ref}
+        />
+    );
+});
