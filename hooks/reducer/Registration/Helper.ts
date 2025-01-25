3feacb697cbb5927c2de52c76130b261d4b0e959
Christian Sturm <christian.sturm@code.berlin>
2025-01-25 17:11:08 +0100

    fix: remove redundant function helper

--- hooks/reducer/Registration/Helper.ts
+++ hooks/reducer/Registration/Helper.ts
@@ -19,11 +19,4 @@ export function areAnyFieldsBeingEdited(
     );
 }
 
-export function areAnyFieldsPredicate<T extends Record<string, unknown>>(
-    obj: T,
-    predicate: (value: T[keyof T]) => boolean,
-) {
-    return Object.values<T[keyof T]>(obj as { [key: string]: T[keyof T] }).some(
-        predicate,
-    );
 }
